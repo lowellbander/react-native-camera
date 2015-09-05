03c722b58f2265ba7a73976db8dea3fb8cc7c567
Lowell Bander <lowellb@lowellb-mbp.local>
2015-09-05 12:44:35 -0400

    takes photos

--- index.ios.js
+++ index.ios.js
@@ -1,42 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  TouchableHighlight
 } = React;
+var Camera = require('react-native-camera');
 
 var AwesomeProject = React.createClass({
-  render: function() {
+  getInitialState() {
+    return {
+      cameraType: Camera.constants.Type.back
+    }
+  },
+
+  render() {
+
     return (
-      <View style={styles.container}>
+      <Camera
+        ref="cam"
+        style={styles.container}
+        onBarCodeRead={this._onBarCodeRead}
+        type={this.state.cameraType}
+      >
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.ios.js{'\n'}
+          Press Cmd+R to reload
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <TouchableHighlight onPress={this._switchCamera}>
+          <Text>The old switcheroo</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._takePicture}>
+          <Text>Take Picture</Text>
+        </TouchableHighlight>
+      </Camera>
     );
+  },
+  _onBarCodeRead(e) {
+    console.log(e);
+  },
+  _switchCamera() {
+    var state = this.state;
+    state.cameraType = state.cameraType === Camera.constants.Type.back
+      ? Camera.constants.Type.front : Camera.constants.Type.back;
+    this.setState(state);
+  },
+  _takePicture() {
+    this.refs.cam.capture(function(err, data) {
+      console.log(err, data);
+    });
   }
 });
+
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'transparent',
   },
   welcome: {
     fontSize: 20,
@@ -46,7 +72,6 @@ var styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
   },
 });
 
